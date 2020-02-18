95d7b2e233479f434d3f25940886528fdab836c4
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-02-18 18:31:38 +0700

    MyBroadcastReceiver

--- MyService/app/src/main/java/com/dicoding/myservice/MainActivity.java
+++ MyService/app/src/main/java/com/dicoding/myservice/MainActivity.java
@@ -2,13 +2,76 @@ package com.dicoding.myservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnStartService;
+    private Button btnStartIntentService;
+    private Button btnStartBoundService;
+    private Button btnStopBoundService;
+    private boolean mServiceBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnStartService = findViewById(R.id.btn_start_service);
+        btnStartService.setOnClickListener(this);
+        btnStartIntentService = findViewById(R.id.btn_start_intent_service);
+        btnStartIntentService.setOnClickListener(this);
+        btnStartBoundService = findViewById(R.id.btn_start_bound_service);
+        btnStartBoundService.setOnClickListener(this);
+        btnStopBoundService = findViewById(R.id.btn_stop_bound_service);
+        btnStopBoundService.setOnClickListener(this);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mServiceBound) {
+            unbindService(mServiceConnection);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_start_service:
+                Intent mStartServiceIntent  = new Intent(MainActivity.this, MyService.class);
+                startService(mStartServiceIntent );
+                break;
+            case R.id.btn_start_intent_service:
+                Intent mStartIntentService = new Intent(MainActivity.this, MyIntentService.class);
+                mStartIntentService.putExtra(MyIntentService.EXTRA_DURATION, 5000L);
+                startService(mStartIntentService);
+                break;
+            case R.id.btn_start_bound_service:
+                Intent mBountStartService = new Intent(MainActivity.this, MyBoundService.class);
+                bindService(mBountStartService, mServiceConnection, BIND_AUTO_CREATE);
+                break;
+            case R.id.btn_stop_bound_service:
+                unbindService(mServiceConnection);
+                break;
+        }
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MyBoundService.MyBinder myBinder = (MyBoundService.MyBinder) service;
+            myBinder.getService();
+            mServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
+    };
 }
