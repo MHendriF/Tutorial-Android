de390183922e8e5e98a3a2a81d7b634a867f423c
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-02-18 20:41:09 +0700

    update

--- MyBroadcastReceiver/app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.java
+++ MyBroadcastReceiver/app/src/main/java/com/dicoding/mybroadcastreceiver/MainActivity.java
@@ -6,8 +6,13 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -16,21 +21,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button btnCheckPermission;
     final int SMS_REQUEST_CODE = 101;
+    private Button btnDownload;
+    public static final String ACTION_DOWNLOAD_STATUS = "download_status";
+    private BroadcastReceiver downloadReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         btnCheckPermission = findViewById(R.id.btn_permission);
         btnCheckPermission.setOnClickListener(this);
+        btnDownload = findViewById(R.id.btn_download);
+        btnDownload.setOnClickListener(this);
+
+        downloadReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(DownloadService.TAG, "Download Selesai");
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show();
+            }
+        };
+        IntentFilter downloadIntentFilter = new IntentFilter(ACTION_DOWNLOAD_STATUS);
+        registerReceiver(downloadReceiver, downloadIntentFilter);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_permission) {
             PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE);
+        }
+        else if (v.getId() == R.id.btn_download) {
+            Intent downloadServiceIntent = new Intent(this, DownloadService.class);
+            startService(downloadServiceIntent);
         }
     }
 
@@ -46,10 +69,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public static class PermissionManager {
-        public static void check(Activity activity, String permission, int requestCode) {
+        private static void check(Activity activity, String permission, int requestCode) {
             if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(activity, new String[]{permission}, requestCode);
             }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (downloadReceiver != null) {
+            unregisterReceiver(downloadReceiver);
         }
     }
 }
