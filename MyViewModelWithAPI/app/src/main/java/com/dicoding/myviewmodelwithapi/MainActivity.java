c105a171f5647f1c965df3d420b78c2c410671c4
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-02-21 15:27:54 +0700

    view model save data when rotate

--- MyViewModelWithAPI/app/src/main/java/com/dicoding/myviewmodelwithapi/MainActivity.java
+++ MyViewModelWithAPI/app/src/main/java/com/dicoding/myviewmodelwithapi/MainActivity.java
@@ -1,14 +1,84 @@
 package com.dicoding.myviewmodelwithapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WeatherAdapter adapter;
+    private EditText edtCity;
+    private ProgressBar progressBar;
+    private Button btnCity;
+    private MainViewModel mainViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtCity = findViewById(R.id.editCity);
+        progressBar = findViewById(R.id.progressBar);
+        btnCity = findViewById(R.id.btnCity);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new WeatherAdapter();
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
+
+        mainViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(MainViewModel.class);
+
+        btnCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city =  edtCity.getText().toString();
+                if (TextUtils.isEmpty(city)) return;
+                showLoading(true);
+
+                mainViewModel.setWeather(city);
+            }
+        });
+
+        mainViewModel.getWeathers().observe(this, new Observer<ArrayList<WeatherItems>>() {
+            @Override
+            public void onChanged(ArrayList<WeatherItems> weatherItems) {
+                if (weatherItems != null){
+                    adapter.setData(weatherItems);
+                    showLoading(false);
+                }
+            }
+        });
+
     }
+
+    private void showLoading(boolean state){
+        if (state){
+            progressBar.setVisibility(View.VISIBLE);
+        }else {
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
 }
