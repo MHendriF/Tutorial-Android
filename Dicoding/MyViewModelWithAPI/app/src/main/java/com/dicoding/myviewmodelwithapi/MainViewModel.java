a69544cf922e6d6206ba2bab61c9d978e7f8585a
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-07 11:50:44 +0700

    restructure project directory

---
+++ Dicoding/MyViewModelWithAPI/app/src/main/java/com/dicoding/myviewmodelwithapi/MainViewModel.java
@@ -0,0 +1,67 @@
+package com.dicoding.myviewmodelwithapi;
+
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainViewModel extends ViewModel {
+
+    private MutableLiveData<ArrayList<WeatherItems>> listWeathers = new MutableLiveData<>();
+
+    void setWeather(final String cities){
+        final ArrayList<WeatherItems> listItems = new ArrayList<>();
+        String apiKey = "9f801b5f86e4bac711cfb8c9f4814a7d";
+        String url = "https://api.openweathermap.org/data/2.5/group?id=" + cities + "&units=metric&appid=" + apiKey;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    //parsing json
+                    String result = new String(responseBody);
+                    JSONObject responseObject = new JSONObject(result);
+                    JSONArray list = responseObject.getJSONArray("list");
+                    for (int i = 0; i < list.length(); i++) {
+                        JSONObject weather = list.getJSONObject(i);
+                        WeatherItems weatherItems = new WeatherItems();
+                        weatherItems.setId(weather.getInt("id"));
+                        weatherItems.setName(weather.getString("name"));
+                        weatherItems.setCurrentWeather(weather.getJSONArray("weather").getJSONObject(0).getString("main"));
+                        weatherItems.setDescription(weather.getJSONArray("weather").getJSONObject(0).getString("description"));
+                        double tempInKelvin = weather.getJSONObject("main").getDouble("temp");
+                        double tempInCelsius = tempInKelvin - 273;
+                        weatherItems.setTemperature(new DecimalFormat("##.##").format(tempInCelsius));
+                        listItems.add(weatherItems);
+                    }
+                    listWeathers.postValue(listItems);
+                    //set data ke adapter
+                    //adapter.setData(listItems);
+                    //showLoading(false);
+                } catch (Exception e) {
+                    Log.d("Exception", e.getMessage());
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Log.d("onFailure", error.getMessage());
+            }
+        });
+    }
+
+    LiveData<ArrayList<WeatherItems>> getWeathers() {
+        return listWeathers;
+    }
+}
