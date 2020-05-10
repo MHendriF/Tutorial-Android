1a499ccdf24deb70e4d24d659f50e0536d4f35cb
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-10 12:54:03 +0700

    part 2

--- CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/Common/SplashScreen.java
+++ CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/Common/SplashScreen.java
@@ -2,17 +2,47 @@ package com.taimoorsikander.cityguideapp.Common;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.taimoorsikander.cityguideapp.R;
+import com.taimoorsikander.cityguideapp.User.UserDashboard;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_TIMER = 5000;
+    ImageView ivBackground;
+    TextView tvPoweredByLine;
+    Animation sideAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
+
+        ivBackground = findViewById(R.id.background_image);
+        tvPoweredByLine = findViewById(R.id.powered_by_line);
+
+        sideAnim = AnimationUtils.loadAnimation(this, R.anim.side_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        ivBackground.setAnimation(sideAnim);
+        tvPoweredByLine.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, UserDashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIMER);
     }
 }
