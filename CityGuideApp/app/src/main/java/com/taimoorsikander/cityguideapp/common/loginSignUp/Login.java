c7f3f085463f34121fb76d08642b243fcf0d4111
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-11 14:28:24 +0700

    Part 8 - Material Design Login Screen

---
+++ CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/common/loginSignUp/Login.java
@@ -0,0 +1,31 @@
+package com.taimoorsikander.cityguideapp.common.loginSignUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.taimoorsikander.cityguideapp.R;
+import com.taimoorsikander.cityguideapp.user.AllCategories;
+
+public class Login extends AppCompatActivity {
+
+    ImageView btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_retailer_login);
+
+        btnBack = findViewById(R.id.iv_back_button);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login.super.onBackPressed();
+            }
+        });
+    }
+}
--- CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/common/loginSignUp/RetailerStartUpScreen.java
+++ CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/common/loginSignUp/RetailerStartUpScreen.java
@@ -2,7 +2,12 @@ package com.taimoorsikander.cityguideapp.common.loginSignUp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.taimoorsikander.cityguideapp.R;
@@ -14,5 +19,19 @@ public class RetailerStartUpScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_retailer_start_up_screen);
+    }
+
+    public void callLoginScreen(View view) {
+        Intent intent = new Intent(getApplicationContext(),  Login.class);
+
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.btn_login), "transition_login");
+
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(RetailerStartUpScreen.this, pairs);
+        startActivity(intent, options.toBundle());
+    }
+
+    public void callSingUpScreen(View view) {
+
     }
 }
