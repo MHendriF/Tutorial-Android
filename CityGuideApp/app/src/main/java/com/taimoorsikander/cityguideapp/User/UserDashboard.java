00c1dea8d14a7a460b60e2a300658ca4a3431b1e
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-11 10:44:26 +0700

    Part 5, Android Navigation Drawer Menu

--- CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/User/UserDashboard.java
+++ CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/User/UserDashboard.java
@@ -1,13 +1,21 @@
 package com.taimoorsikander.cityguideapp.User;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.google.android.material.navigation.NavigationView;
 import com.taimoorsikander.cityguideapp.HelperClasses.HomeAdapter.Categories;
 import com.taimoorsikander.cityguideapp.HelperClasses.HomeAdapter.CategoriesAdapter;
 import com.taimoorsikander.cityguideapp.HelperClasses.HomeAdapter.FeaturedAdapter;
@@ -18,7 +26,7 @@ import com.taimoorsikander.cityguideapp.R;
 
 import java.util.ArrayList;
 
-public class UserDashboard extends AppCompatActivity {
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     RecyclerView rvFeatured;
     RecyclerView rvMostViewed;
@@ -27,6 +35,13 @@ public class UserDashboard extends AppCompatActivity {
     MostViewedAdapter mostViewedAdapter;
     CategoriesAdapter categoriesAdapter;
     private GradientDrawable gradient1, gradient2, gradient3, gradient4;
+
+    static final float END_SCALE = 0.7f;
+    ImageView menuIcon;
+    LinearLayout contentView;
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +52,14 @@ public class UserDashboard extends AppCompatActivity {
         rvFeatured = findViewById(R.id.rv_featured);
         rvMostViewed = findViewById(R.id.rv_most_viewed);
         rvCategories = findViewById(R.id.rv_categories);
+
+        menuIcon = findViewById(R.id.iv_menu_icon);
+        contentView = findViewById(R.id.ll_content);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        naviagtionDrawer();
 
         featuredRecycler();
         mostViewedRecycler();
@@ -91,5 +114,60 @@ public class UserDashboard extends AppCompatActivity {
 
         GradientDrawable gradient1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, new int[]{0xffeff400, 0xffaf600});
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
+
+    private void naviagtionDrawer(){
+
+        //Naviagtion Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        animateNavigationDrawer();
+    }
+
+    private void animateNavigationDrawer() {
+
+        //Add any color or remove it to use the default one!
+        //To make it transparent use Color.Transparent in side setScrimColor();
+        //drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.setScrimColor(getResources().getColor(R.color.colorPrimary));
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
+    }
+
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else
+            super.onBackPressed();
     }
 }
