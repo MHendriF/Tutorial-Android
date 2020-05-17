e16770fbdeb21e1f81055ef7a195efea2f42b588
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-17 15:15:40 +0700

    toast, snackbar, material design

--- SkillAcademy/Belajar/app/src/main/java/id/skillacademy/belajar/MainActivity.kt
+++ SkillAcademy/Belajar/app/src/main/java/id/skillacademy/belajar/MainActivity.kt
@@ -2,11 +2,23 @@ package id.skillacademy.belajar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.btn_click)
+        button.setOnClickListener { Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show() }
+
+        val rootLayout = findViewById<LinearLayout>(R.id.root_layout)
+        val btn_snack = findViewById<Button>(R.id.btn_snack)
+        btn_snack.setOnClickListener { Snackbar.make(rootLayout, "From Snack", Snackbar.LENGTH_SHORT).show() }
     }
 }
