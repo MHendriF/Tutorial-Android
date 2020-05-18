aedee512751342f6c5b9aa1488c564c86f094c49
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-18 11:19:28 +0700

    using kotlin extension

--- SkillAcademy/MVPArchitecture/app/src/main/java/id/skillacademy/mvparchitecture/MainActivity.kt
+++ SkillAcademy/MVPArchitecture/app/src/main/java/id/skillacademy/mvparchitecture/MainActivity.kt
@@ -7,34 +7,30 @@ import android.view.View
 import android.widget.ProgressBar
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), HomeView {
-
-    private lateinit var progressBar: ProgressBar
-    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        progressBar = findViewById(R.id.pb_home)
-        recyclerView = findViewById(R.id.rv_home)
 
         val presenter = HomePresenter(this)
         presenter.discoverMovie()
     }
 
     override fun onShowLoading() {
-        progressBar.visibility = View.VISIBLE
+        pb_home.visibility = View.VISIBLE
     }
 
     override fun onHideLoading() {
-        progressBar.visibility = View.GONE
+        pb_home.visibility = View.GONE
+        rv_home.visibility = View.VISIBLE
     }
 
     override fun onResponse(results: List<Result>) {
-        recyclerView.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
-        recyclerView.adapter = HomeAdapter(results)
+        rv_home.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+        rv_home.adapter = HomeAdapter(results)
     }
 
     override fun onFailure(error: Throwable) {
