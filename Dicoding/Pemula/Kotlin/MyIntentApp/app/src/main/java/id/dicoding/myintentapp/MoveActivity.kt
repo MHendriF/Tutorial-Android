5c1a84446b22a124925d0495410f0094d74d03fb
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-23 15:11:05 +0700

    intent done

---
+++ Dicoding/Pemula/Kotlin/MyIntentApp/app/src/main/java/id/dicoding/myintentapp/MoveActivity.kt
@@ -0,0 +1,13 @@
+package id.dicoding.myintentapp
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MoveActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
