b1f37c38b56979027cfb030f9ed34400fb0b0a36
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-05-23 16:12:53 +0700

    Recycler View done

---
+++ Dicoding/Pemula/Kotlin/MyRecyclerView/app/src/main/java/id/dicoding/myrecyclerview/adapter/GridHeroAdapter.kt
@@ -0,0 +1,49 @@
+package id.dicoding.myrecyclerview.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import id.dicoding.myrecyclerview.R
+import id.dicoding.myrecyclerview.model.Hero
+
+class GridHeroAdapter(private val listHeroes: ArrayList<Hero>) : RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): GridHeroAdapter.GridViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_grid_hero, parent, false)
+        return GridViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listHeroes.size
+    }
+
+    override fun onBindViewHolder(holder: GridHeroAdapter.GridViewHolder, position: Int) {
+        Glide.with(holder.itemView.context)
+            .load(listHeroes[position].photo)
+            .apply(RequestOptions().override(350, 550))
+            .into(holder.imgPhoto)
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHeroes[holder.adapterPosition]) }
+
+    }
+
+    inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
+    }
+
+}
---
+++ Dicoding/Pemula/Kotlin/MyRecyclerView/app/src/main/java/id/dicoding/myrecyclerview/model/Hero.kt
@@ -0,0 +1,7 @@
+package id.dicoding.myrecyclerview.model
+
+data class Hero(
+    var name: String = "",
+    var detail: String = "",
+    var photo: Int = 0
+)
