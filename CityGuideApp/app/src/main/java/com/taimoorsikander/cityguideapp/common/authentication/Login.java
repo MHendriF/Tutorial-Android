a93d88a381d9ce65fbda3abf2c2a467c6986175c
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-06-25 09:51:59 +0700

    Custom loading Progress Bar in android studio

--- CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/common/authentication/Login.java
+++ CityGuideApp/app/src/main/java/com/taimoorsikander/cityguideapp/common/authentication/Login.java
@@ -6,14 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 
+import com.google.android.material.textfield.TextInputLayout;
+import com.hbb20.CountryCodePicker;
 import com.taimoorsikander.cityguideapp.R;
 import com.taimoorsikander.cityguideapp.user.UserDashboard;
 
 public class Login extends AppCompatActivity {
 
     ImageView btnBack;
+    TextInputLayout phoneNumber, password;
+    CountryCodePicker countryCodePicker;
+    Button btnLogin, btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,12 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_retailer_login);
 
         btnBack = findViewById(R.id.iv_back_button);
+        countryCodePicker = findViewById(R.id.login_country_code_picker);
+        phoneNumber = findViewById(R.id.login_phone_number);
+        password = findViewById(R.id.login_password);
+        btnLogin = findViewById(R.id.btn_login);
+        btnSignUp = findViewById(R.id.btn_sign_up);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
