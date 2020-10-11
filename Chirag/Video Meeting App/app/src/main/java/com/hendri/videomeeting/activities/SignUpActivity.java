ddee68201a613d2e2958537c335e04b0941337b6
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-10-11 21:06:45 +0700

    signup data to firebase firestore ok

--- Chirag/Video Meeting App/app/src/main/java/com/hendri/videomeeting/activities/SignUpActivity.java
+++ Chirag/Video Meeting App/app/src/main/java/com/hendri/videomeeting/activities/SignUpActivity.java
@@ -4,17 +4,92 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.hendri.videomeeting.R;
+import com.hendri.videomeeting.utilities.Constants;
+import com.hendri.videomeeting.utilities.PreferenceManager;
+
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText inputFirstName, inputLastName, inputEmail, inputPassword, inputConfirmPassword;
+    private Button btnSignUp;
+    private ProgressBar signUpProgressBar;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        preferenceManager = new PreferenceManager(getApplicationContext());
+
         findViewById(R.id.ivBack).setOnClickListener(view -> onBackPressed());
         findViewById(R.id.tvSignIn).setOnClickListener(view -> onBackPressed());
+
+        inputFirstName = findViewById(R.id.inputFirstName);
+        inputLastName = findViewById(R.id.inputLastName);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputConfirmPassword = findViewById(R.id.inputConfirmPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        signUpProgressBar = findViewById(R.id.signUpProgressBar);
+
+        btnSignUp.setOnClickListener(view -> {
+            if(inputFirstName.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignUpActivity.this, "Enter first name", Toast.LENGTH_SHORT).show();
+            } else if(inputLastName.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignUpActivity.this, "Enter last name", Toast.LENGTH_SHORT).show();
+            } else if(inputEmail.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignUpActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+            } else if(!Patterns.EMAIL_ADDRESS.matcher(inputEmail.getText().toString()).matches()) {
+                Toast.makeText(SignUpActivity.this, "Enter valid email", Toast.LENGTH_SHORT).show();
+            } else if(inputPassword.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignUpActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+            } else if(inputConfirmPassword.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignUpActivity.this, "Confirm your password", Toast.LENGTH_SHORT).show();
+            } else if (!inputPassword.getText().toString().equals(inputConfirmPassword.getText().toString())) {
+                Toast.makeText(SignUpActivity.this, "Password & confirm password must be same", Toast.LENGTH_SHORT).show();
+            } else {
+                signUp();
+            }
+        });
+    }
+
+    private void signUp() {
+        btnSignUp.setVisibility(View.INVISIBLE);
+        signUpProgressBar.setVisibility(View.VISIBLE);
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> user = new HashMap<>();
+        user.put(Constants.KEY_FIRST_NAME, inputFirstName.getText().toString());
+        user.put(Constants.KEY_LAST_NAME, inputLastName.getText().toString());
+        user.put(Constants.KEY_EMAIL, inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD, inputPassword.getText().toString());
+
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {
+                    preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                    preferenceManager.putString(Constants.KEY_FIRST_NAME, inputFirstName.getText().toString());
+                    preferenceManager.putString(Constants.KEY_LAST_NAME, inputLastName.getText().toString());
+                    preferenceManager.putString(Constants.KEY_EMAIL, inputEmail.getText().toString());
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                })
+                .addOnFailureListener(e -> {
+                    signUpProgressBar.setVisibility(View.INVISIBLE);
+                    btnSignUp.setVisibility(View.VISIBLE);
+                    Toast.makeText(SignUpActivity.this, "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
     }
 }
