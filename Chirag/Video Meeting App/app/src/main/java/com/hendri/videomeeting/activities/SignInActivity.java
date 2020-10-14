ffea327031b8e0b4769400d92545854d0cb6846a
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-10-14 20:36:56 +0700

    Sign In Using Firestore & Shared Preferences

--- Chirag/Video Meeting App/app/src/main/java/com/hendri/videomeeting/activities/SignInActivity.java
+++ Chirag/Video Meeting App/app/src/main/java/com/hendri/videomeeting/activities/SignInActivity.java
@@ -1,19 +1,93 @@
 package com.hendri.videomeeting.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.hendri.videomeeting.R;
+import com.hendri.videomeeting.utilities.Constants;
+import com.hendri.videomeeting.utilities.PreferenceManager;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText inputEmail, inputPassword;
+    private MaterialButton btnSignIn;
+    private ProgressBar signInProgressBar;
+    private PreferenceManager preferenceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        preferenceManager = new PreferenceManager(getApplicationContext());
+
+        if (preferenceManager.getBoolean(Constants.KEY_IS_SIGNED_IN)) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         findViewById(R.id.tvSignUp).setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
 
-     }
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        signInProgressBar = findViewById(R.id.signInProgressBar);
+
+        btnSignIn.setOnClickListener(view -> {
+            if (inputEmail.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignInActivity.this, "Enter email", Toast.LENGTH_SHORT).show();
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(inputEmail.getText().toString()).matches()) {
+                Toast.makeText(SignInActivity.this, "Enter valid email", Toast.LENGTH_SHORT).show();
+            } else if (inputPassword.getText().toString().trim().isEmpty()) {
+                Toast.makeText(SignInActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
+            } else {
+                signIn();
+            }
+        });
+    }
+
+    private void signIn() {
+        btnSignIn.setVisibility(View.INVISIBLE);
+        signInProgressBar.setVisibility(View.VISIBLE);
+
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS)
+                .whereEqualTo(Constants.KEY_EMAIL, inputEmail.getText().toString())
+                .whereEqualTo(Constants.KEY_PASSWORD, inputPassword.getText().toString())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() >0 ){
+                        DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+                        preferenceManager.putBoolean(Constants.KEY_IS_SIGNED_IN, true);
+                        preferenceManager.putString(Constants.KEY_FIRST_NAME, documentSnapshot.getString(Constants.KEY_FIRST_NAME));
+                        preferenceManager.putString(Constants.KEY_LAST_NAME, documentSnapshot.getString(Constants.KEY_LAST_NAME));
+                        preferenceManager.putString(Constants.KEY_EMAIL, documentSnapshot.getString(Constants.KEY_EMAIL));
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    } else {
+                        signInProgressBar.setVisibility(View.INVISIBLE);
+                        btnSignIn.setVisibility(View.VISIBLE);
+                        Toast.makeText(SignInActivity.this, "Unable to sign in!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
 }
