4d25b752ef6b91b19b564ed9ff4a57dc638d0273
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-10-19 13:52:55 +0700

    Displaying Image Slider Using ViewPager2 & Data Binding

--- Chirag/TV Show/app/src/main/java/com/hendri/tvshow/activities/TVShowDetailsActivity.java
+++ Chirag/TV Show/app/src/main/java/com/hendri/tvshow/activities/TVShowDetailsActivity.java
@@ -1,13 +1,20 @@
 package com.hendri.tvshow.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.hendri.tvshow.R;
+import com.hendri.tvshow.adapters.ImageSliderAdapter;
 import com.hendri.tvshow.databinding.ActivityTVShowDetailsBinding;
 import com.hendri.tvshow.viewmodels.TVShowDetailsViewModel;
 
@@ -33,9 +40,63 @@ public class TVShowDetailsActivity extends AppCompatActivity {
         String tvShowId = String.valueOf(getIntent().getIntExtra("id", -1));
         tvShowDetailsViewModel.getTVShowDetails(tvShowId).observe(
                 this, tvShowDetailsResponse -> {
-                    activityTVShowDetailsBinding.setIsLoading(true);
-                    Toast.makeText(getApplicationContext(), tvShowDetailsResponse.getTvShowDetails().getUrl(), Toast.LENGTH_SHORT).show();
+                    activityTVShowDetailsBinding.setIsLoading(false);
+                    if (tvShowDetailsResponse.getTvShowDetails() != null) {
+                        if (tvShowDetailsResponse.getTvShowDetails().getPictures() != null) {
+                            loadImageSlider(tvShowDetailsResponse.getTvShowDetails().getPictures());
+                        }
+                    }
                 }
         );
+    }
+
+    private void loadImageSlider(String[] sliderImages) {
+        activityTVShowDetailsBinding.sliderViewPager.setOffscreenPageLimit(1);
+        activityTVShowDetailsBinding.sliderViewPager.setAdapter(new ImageSliderAdapter(sliderImages));
+        activityTVShowDetailsBinding.sliderViewPager.setVisibility(View.VISIBLE);
+        activityTVShowDetailsBinding.viewFadingEdge.setVisibility(View.VISIBLE);
+        setupSliderIndicators(sliderImages.length);
+        activityTVShowDetailsBinding.sliderViewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentSliderIndicator(position);
+            }
+        });
+    }
+
+    private void setupSliderIndicators(int count) {
+        ImageView[] indicators = new ImageView[count];
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(0, 0, 0, 0);
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.bg_slider_indicator_inactive
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            activityTVShowDetailsBinding.layoutSliderIndicators.addView(indicators[i]);
+        }
+        activityTVShowDetailsBinding.layoutSliderIndicators.setVisibility(View.VISIBLE);
+        setCurrentSliderIndicator(0);
+    }
+
+    private void setCurrentSliderIndicator(int position) {
+        int childCount = activityTVShowDetailsBinding.layoutSliderIndicators.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) activityTVShowDetailsBinding.layoutSliderIndicators.getChildAt(i);
+            if (i == position) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.bg_slider_indicator_active)
+                );
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.bg_slider_indicator_inactive)
+                );
+            }
+        }
     }
 }
