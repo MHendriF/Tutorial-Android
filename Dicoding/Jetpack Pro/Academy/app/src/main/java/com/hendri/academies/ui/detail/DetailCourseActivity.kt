10a7b4237c959b30ec9e1788327137fc74ce6441
Hendri Febriansyah <hendrifebriansyah28@gmail.com>
2020-10-25 20:14:35 +0700

    layouting

--- Dicoding/Jetpack Pro/Academy/app/src/main/java/com/hendri/academies/ui/detail/DetailCourseActivity.kt
+++ Dicoding/Jetpack Pro/Academy/app/src/main/java/com/hendri/academies/ui/detail/DetailCourseActivity.kt
@@ -5,18 +5,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.hendri.academies.R
+import kotlinx.android.synthetic.main.activity_detail_course.*
 
 class DetailCourseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_course)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
