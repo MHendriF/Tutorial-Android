db304936a76f191f6874ba74bf0a23631f4df2b3
MHendriF <hendrifebriansyah28@gmail.com>
2020-12-26 17:44:40 +0700

    simple dynamic feature done

--- Dicoding/Expert/MySimpleLogin/dynamic-feature/app/src/main/java/com/mhendrif/mysimplelogin/HomeActivity.kt
+++ Dicoding/Expert/MySimpleLogin/dynamic-feature/app/src/main/java/com/mhendrif/mysimplelogin/HomeActivity.kt
@@ -3,7 +3,10 @@ package com.mhendrif.mysimplelogin
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 import com.mhendrif.core.SessionManager
 import com.mhendrif.core.UserRepository
 import com.mhendrif.mysimplelogin.databinding.ActivityHomeBinding
@@ -27,10 +30,43 @@ class HomeActivity : AppCompatActivity() {
             userRepository.logoutUser()
             moveToMainActivity()
         }
+
+        binding.fab.setOnClickListener {
+            try {
+                installChatModule()
+            } catch (e: Exception){
+                Toast.makeText(this, "Module not found", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun moveToMainActivity() {
         startActivity(Intent(this, MainActivity::class.java))
         finish()
+    }
+
+    private fun installChatModule() {
+        val splitInstallManager = SplitInstallManagerFactory.create(this)
+        val moduleChat = "chat"
+        if (splitInstallManager.installedModules.contains(moduleChat)) {
+            moveToChatActivity()
+            Toast.makeText(this, "Open module", Toast.LENGTH_SHORT).show()
+        } else {
+            val request = SplitInstallRequest.newBuilder()
+                .addModule(moduleChat)
+                .build()
+            splitInstallManager.startInstall(request)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Success installing module", Toast.LENGTH_SHORT).show()
+                    moveToChatActivity()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Error installing module", Toast.LENGTH_SHORT).show()
+                }
+        }
+    }
+
+    private fun moveToChatActivity() {
+        startActivity(Intent(this, Class.forName("com.mhendrif.mysimplelogin.chat.ChatActivity")))
     }
 }
