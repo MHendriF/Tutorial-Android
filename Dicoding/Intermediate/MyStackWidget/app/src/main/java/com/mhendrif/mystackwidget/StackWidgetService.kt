3b3643cee0a3baa49fa98f0b7e3fd766d3b40a80
MHendriF <hendrifebriansyah28@gmail.com>
2023-10-24 11:39:30 +0700

    mystackwidget

---
+++ Dicoding/Intermediate/MyStackWidget/app/src/main/java/com/mhendrif/mystackwidget/StackWidgetService.kt
@@ -0,0 +1,11 @@
+package com.mhendrif.mystackwidget
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.widget.RemoteViewsService
+
+class StackWidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(intent: Intent): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+}
