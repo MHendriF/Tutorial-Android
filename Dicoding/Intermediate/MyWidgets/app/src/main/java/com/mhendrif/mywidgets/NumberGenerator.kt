9066a2cb87b57e4374ea5663bdb976101eded6d9
MHendriF <hendrifebriansyah28@gmail.com>
2023-10-24 10:27:11 +0700

    done

---
+++ Dicoding/Intermediate/MyWidgets/app/src/main/java/com/mhendrif/mywidgets/NumberGenerator.kt
@@ -0,0 +1,11 @@
+package com.mhendrif.mywidgets
+
+import java.util.Random
+
+
+internal object NumberGenerator {
+    fun generate(max: Int): Int {
+        val random = Random()
+        return random.nextInt(max)
+    }
+}
